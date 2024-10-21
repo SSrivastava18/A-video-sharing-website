a275ecbac10cc4fb5f81026c9c59538172cddbe6
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-10-21 12:12:25 +0530

    Sidebar

--- App.jsx
+++ App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +12,10 @@ function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path = '/' element={<Home/>} />
+        <Route path = '/video/:categoryId/:videoId' element = {<Video/>} />
+      </Routes>
     </div>
     
   )
