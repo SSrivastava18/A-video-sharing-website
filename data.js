2d25566e6aad63c94fa9181eaa277b0cd17b649c
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-10-24 00:45:05 +0530

    sidebar and feed api

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
 export const API_KEY = 'AIzaSyACsZvq0AFR261DK-g58F7oUeidUIBVVs0';
+
+export const value_converter = (value) =>{
+    if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if (value>1000) {
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+}
