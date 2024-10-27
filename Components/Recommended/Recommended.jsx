9ce17cd344fcddc85496e938c4d7a4528c3ddaf3
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-10-27 14:29:07 +0530

    recommendation

--- Components/Recommended/Recommended.jsx
+++ Components/Recommended/Recommended.jsx
@@ -1,68 +1,42 @@
-import React from 'react'
-import './Recommended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
+import React, { useState, useEffect } from 'react';
+import './Recommended.css';
+import { API_KEY, value_converter } from '../../data';
 
+const Recommended = ({ categoryId }) => {
+    const [apiData, setApiData] = useState([]);
 
-const Recommended = () => {
-  return (
-    <div className='recommended'>
-     <div className="side-video-list">
-        <img src={thumbnail1}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-     <div className="side-video-list">
-        <img src={thumbnail2}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-     <div className="side-video-list">
-        <img src={thumbnail3}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-     <div className="side-video-list">
-        <img src={thumbnail2}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-     <div className="side-video-list">
-        <img src={thumbnail1}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-     <div className="side-video-list">
-        <img src={thumbnail3}   alt="" />
-        <div className='vid-info'>
-            <h4>Know about Yourself.</h4>
-            <p>Saurabh Srivastava</p>
-            <p>200k Views</p>
-        </div>
-     </div>
-    </div>
-  )
-}
+    const fetchData = async () => {
+        try {
+            const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+            const response = await fetch(relatedVideoUrl);
+            const data = await response.json();
+            setApiData(data.items || []); // Fallback to an empty array if data.items is undefined
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
-export default Recommended
+    useEffect(() => {
+        fetchData();
+    }, [categoryId]);
+
+    return (
+        <div className='recommended'>
+             
+                {apiData.map((item, index) =>{
+                    return(
+                    <div key={index} className="side-video-list">
+                        <img src={item.snippet.thumbnails.medium.url} alt=''/>
+                        <div className='vid-info'>
+                            <h4>{item.snippet.title}</h4>
+                            <p>{item.snippet.channelTitle}</p>
+                            <p>{value_converter(item.statistics.viewCount)} Views</p>
+                        </div>
+                    </div>
+                    )
+            })}
+        </div>
+    );
+};
+
+export default Recommended;
